95d783976e1bf58700db2f502fc83e6ff90353d5
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-08-27 11:01:28 +0200

    FEAT: Add ArtistCreationRequestDTO schema validation

--- services/catalog-command-service/src/main/java/it/musicplatform/catalogcommandservice/dto/artist/ArtistCreationRequestDTO.java
+++ services/catalog-command-service/src/main/java/it/musicplatform/catalogcommandservice/dto/artist/ArtistCreationRequestDTO.java
@@ -1,5 +1,6 @@
 package it.musicplatform.catalogcommandservice.dto.artist;
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -8,6 +9,14 @@ import java.time.LocalDate;
 @Getter @Setter
 @ToString
 public class ArtistCreationRequestDTO {
+    @NotBlank(message = "Artist's name can't be empty")
+    @Pattern(
+            regexp = "^[A-Za-z0-9_]{3,20}$",
+            message = "Name must be 3–20 characters long and contain only letters, numbers, or underscores"
+    )
     private String name;
+
+    @NotNull(message = "Foundation date is mandatory.")
+    @PastOrPresent(message = "Foundation date can't be in the future.")
     private LocalDate foundationDate;
 }
