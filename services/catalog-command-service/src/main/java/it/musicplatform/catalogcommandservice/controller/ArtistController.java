38d780ae89cb8a0c78dbc4c1e1f2c9c447e2b94c
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-08-28 11:26:32 +0200

    FEAT: Add logging for artist creation and update operations

--- services/catalog-command-service/src/main/java/it/musicplatform/catalogcommandservice/controller/ArtistController.java
+++ services/catalog-command-service/src/main/java/it/musicplatform/catalogcommandservice/controller/ArtistController.java
@@ -4,6 +4,7 @@ import it.musicplatform.catalogcommandservice.dto.artist.*;
 import it.musicplatform.catalogcommandservice.service.ArtistCommandService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -11,13 +12,15 @@ import java.util.UUID;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController @RequestMapping("/artists")
-@RequiredArgsConstructor
+@RequiredArgsConstructor @Slf4j
 public class ArtistController {
     private final ArtistCommandService artistCommandService;
 
     @PutMapping(path = "/{artistId}", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     public ArtistSummaryResponseDTO updateArtist(@PathVariable UUID artistId,
             @RequestBody @Valid ArtistCreationRequestDTO creationRequestDTO) {
-        return artistCommandService.updateArtist(artistId, creationRequestDTO);
+        ArtistSummaryResponseDTO artist = artistCommandService.updateArtist(artistId, creationRequestDTO);
+        log.info("Artist updated: {}.", artist);
+        return artist;
     }
 }
