ce9c348018638d7467cc666cd2caaefde840b228
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-09-04 13:00:03 +0200

    FEAT: Add schemas for entity Album

---
+++ services/catalog-command-service/src/main/java/it/musicplatform/catalogcommandservice/dto/album/AlbumSummaryResponseDTO.java
@@ -0,0 +1,20 @@
+package it.musicplatform.catalogcommandservice.dto.album;
+
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@AllArgsConstructor @NoArgsConstructor
+@Getter @Setter
+@ToString @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class AlbumSummaryResponseDTO {
+    @EqualsAndHashCode.Include private UUID id;
+
+    private String name;
+
+    private LocalDate originalReleaseDate;
+
+    private Integer numberOfSongs;
+}
