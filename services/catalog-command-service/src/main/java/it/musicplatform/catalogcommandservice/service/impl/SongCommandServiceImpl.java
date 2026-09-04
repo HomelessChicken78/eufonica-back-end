b66465738c289a90d5fef8d230440fd4e76c407d
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-09-04 12:00:36 +0200

    FIX: Doesn't return a 500 status code when trying to create a song that doesn't pass the unique constraint (Song.title, Song.artistOwner)

--- services/catalog-command-service/src/main/java/it/musicplatform/catalogcommandservice/service/impl/SongCommandServiceImpl.java
+++ services/catalog-command-service/src/main/java/it/musicplatform/catalogcommandservice/service/impl/SongCommandServiceImpl.java
@@ -60,6 +60,13 @@ public class SongCommandServiceImpl implements SongCommandService {
         // TODO this currently uses artistId. It should use the jwt instead
         song.setArtistOwner(artistService.findByIdOrThrow(ownerId));
 
+        // Check that the song with that title of the same author does not exist
+        Artist owner = artistService.findByIdOrThrow(ownerId);
+        if (songRepository.existsByTitleAndArtistOwner(request.getTitle(), owner)) {
+            log.warn("Song {} for artist {} already exists.", request.getTitle(), owner.getName());
+            throw new ConflictException(String.format("Song %s for artist %s already exists.", request.getTitle(), owner.getName()));
+        }
+
         // Guarantees that the constraint [V.song_owner.IS_A_song_credit] is always satisfied
         request.getCreditedArtists().add(ownerId);
 
