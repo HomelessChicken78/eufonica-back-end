f76ba8c97a9321fc94e91ad657359b6f53b38a84
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-09-04 16:43:29 +0200

    REFACTOR: Remove unused import

--- services/catalog-command-service/src/main/java/it/musicplatform/catalogcommandservice/dto/album/AlbumSummaryResponseDTO.java
+++ services/catalog-command-service/src/main/java/it/musicplatform/catalogcommandservice/dto/album/AlbumSummaryResponseDTO.java
@@ -3,7 +3,6 @@ package it.musicplatform.catalogcommandservice.dto.album;
 import lombok.*;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @AllArgsConstructor @NoArgsConstructor
