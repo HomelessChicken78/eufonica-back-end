01dbc04bfa894114d3646d09a854b990784a1d96
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-09-07 12:52:36 +0200

    REFACTOR: Integrate S3AsyncClient for audio file storage and add S3 bucket configuration

--- services/catalog-command-service/src/main/java/it/eufonica/catalogcommandservice/service/impl/AudioStorageServiceImpl.java
+++ services/catalog-command-service/src/main/java/it/eufonica/catalogcommandservice/service/impl/AudioStorageServiceImpl.java
@@ -3,16 +3,26 @@ package it.eufonica.catalogcommandservice.service.impl;
 import it.eufonica.catalogcommandservice.service.AudioStorageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
 
 @Service @Transactional
 @RequiredArgsConstructor @Slf4j
 public class AudioStorageServiceImpl implements AudioStorageService {
+    private final S3AsyncClient s3AsyncClient;
+
+    @Value("${S3_BUCKET_NAME}")
+    private String bucketS3;
+
+    @Value("${S3_AUDIO_PREFIX:audio/}")
+    private String audioPrefix;
+
     @Override
     public String store(MultipartFile file) {
-        // TODO send to S3
+        // s3AsyncClient.putObject()
 
         return "photos/2026/09/vacation.jpg";
     }
