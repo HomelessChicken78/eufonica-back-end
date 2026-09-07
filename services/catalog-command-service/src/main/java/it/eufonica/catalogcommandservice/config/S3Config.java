0b101dc673bf21a19b75ef2402dd3177cb466a70
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-09-07 11:01:32 +0200

    CHORE: Configure S3 Async Client

---
+++ services/catalog-command-service/src/main/java/it/eufonica/catalogcommandservice/config/S3Config.java
@@ -0,0 +1,30 @@
+package it.eufonica.catalogcommandservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import software.amazon.awssdk.auth.credentials.*;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.*;
+
+import java.time.Duration;
+
+@Configuration
+public class S3Config {
+    @Value("${AWS_ACCESS_KEY_ID}")
+    private String awsAccessKeyId;
+
+    @Value("${AWS_SECRET_ACCESS_KEY}")
+    private String awsAccessKeySecret;
+
+    @Value("${AWS_REGION}")
+    private String awsRegion;
+
+    @Bean
+    public S3AsyncClient s3AsyncClient() {
+        return S3AsyncClient.builder()
+                .region(Region.of(awsRegion))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(awsAccessKeyId, awsAccessKeySecret)))
+                .overrideConfiguration(b -> b.apiCallTimeout(Duration.ofMinutes(2)))
+                .build();
+    }
+}
