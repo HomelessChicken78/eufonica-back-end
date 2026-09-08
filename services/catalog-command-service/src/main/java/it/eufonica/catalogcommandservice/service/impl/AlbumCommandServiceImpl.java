4c89a87e8a16aab178b35cfb3c8a01a02a3b842a
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-09-08 17:08:06 +0200

    FIX: method AlbumCommandServiceImpl.updateAlbum now correctly saves the modified entity

--- services/catalog-command-service/src/main/java/it/eufonica/catalogcommandservice/service/impl/AlbumCommandServiceImpl.java
+++ services/catalog-command-service/src/main/java/it/eufonica/catalogcommandservice/service/impl/AlbumCommandServiceImpl.java
@@ -106,18 +106,22 @@ public class AlbumCommandServiceImpl implements AlbumCommandService {
     public AlbumSummaryResponseDTO updateAlbum(UUID albumId, AlbumCreationRequestDTO request) {
         Album album = findByIdOrThrow(albumId);
 
-        album.getSongs().clear();
-        album.getArtists().clear();
+        Album updatedAlbum = mapper.toEntity(request);
+        updatedAlbum.setId(albumId);
+        updatedAlbum.setPubDate(album.getPubDate());
+
+        updatedAlbum.getSongs().clear();
+        updatedAlbum.getArtists().clear();
 
         // [V.Album.rilascio_originale_prima_di_pubblicazione]
         // album.original_release_date <= album.pub_date
-        if (request.getOriginalReleaseDate().isAfter(album.getPubDate().toLocalDate()))
+        if (request.getOriginalReleaseDate().isAfter(updatedAlbum.getPubDate().toLocalDate()))
             throw new ConflictException("Album's original release date must be before or equal to its publication date.");
 
-        mapArtistsToAlbum(request.getArtists(), album, album.getPubDate());
-        mapSongsToAlbum(request.getSongs(), album);
+        mapArtistsToAlbum(request.getArtists(), updatedAlbum, updatedAlbum.getPubDate());
+        mapSongsToAlbum(request.getSongs(), updatedAlbum);
 
-        Album savedAlbum = albumRepository.save(album);
+        Album savedAlbum = albumRepository.save(updatedAlbum);
         return mapper.toSummaryResponse(savedAlbum);
     }
 
