9e39a1a659e0f1fbd1ff79dbc758e7c94efb4754
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-09-08 12:51:59 +0200

    FEAT: Add findByIdOrThrow method to AlbumCommandService for album retrieval

--- services/catalog-command-service/src/main/java/it/eufonica/catalogcommandservice/service/AlbumCommandService.java
+++ services/catalog-command-service/src/main/java/it/eufonica/catalogcommandservice/service/AlbumCommandService.java
@@ -1,12 +1,20 @@
 package it.eufonica.catalogcommandservice.service;
 
 import it.eufonica.catalogcommandservice.dto.album.*;
+import it.eufonica.catalogcommandservice.exception.client.NotFoundException;
 import it.eufonica.catalogcommandservice.model.Album;
 
 import java.util.UUID;
 
 public interface AlbumCommandService {
-
+    /**
+     * Finds an album by its id.
+     *
+     * @param id the unique id of the album
+     * @return the album associated with the given id
+     * @throws NotFoundException if no album exists with the given id
+     */
+    Album findByIdOrThrow(UUID id);
 
     AlbumSummaryResponseDTO createAlbum(AlbumCreationRequestDTO request);
 
