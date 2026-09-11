696813ce6b15463279465d20192e06ccf46766eb
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-09-11 17:12:22 +0200

    FIX: Change OutboxEventRepository ID type from Integer to UUID

--- services/catalog-command-service/src/main/java/it/eufonica/catalogcommandservice/outbox/OutboxEventRepository.java
+++ services/catalog-command-service/src/main/java/it/eufonica/catalogcommandservice/outbox/OutboxEventRepository.java
@@ -4,11 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
-public interface OutboxEventRepository extends JpaRepository<OutboxEvent, Integer> {
+public interface OutboxEventRepository extends JpaRepository<OutboxEvent, UUID> {
     @Query(
             """
             SELECT evt
