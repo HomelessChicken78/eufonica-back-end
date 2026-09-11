f06b25ec70a271145a3f72d659aad58a6cc24600
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-09-11 14:58:57 +0200

    FEAT: Implement OutboxProcessor for scheduled event processing and enable scheduling in application

--- services/catalog-command-service/src/main/java/it/eufonica/catalogcommandservice/CatalogCommandServiceApplication.java
+++ services/catalog-command-service/src/main/java/it/eufonica/catalogcommandservice/CatalogCommandServiceApplication.java
@@ -2,8 +2,9 @@ package it.eufonica.catalogcommandservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication @EnableScheduling
 public class CatalogCommandServiceApplication {
 
     public static void main(String[] args) {
---
+++ services/catalog-command-service/src/main/java/it/eufonica/catalogcommandservice/outbox/OutboxProcessor.java
@@ -0,0 +1,46 @@
+package it.eufonica.catalogcommandservice.outbox;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.time.LocalDateTime.now;
+
+@Component
+@RequiredArgsConstructor @Slf4j
+public class OutboxProcessor {
+    private final OutboxService outboxService;
+    private final KafkaTemplate<?, ?> kafkaTemplate;
+
+    @Scheduled(fixedDelayString = "${OUTBOX_PROCESSOR_JOB_DELAY_MS:1000}")
+    public void process() {
+        log.trace("Outbox processing started.");
+
+        List<OutboxEvent> events = outboxService.findEventsReadyForProcessing(now());
+        LocalDateTime start = now();
+        int successfulEvents = 0;
+        int failedEvents = 0;
+
+        for (OutboxEvent event : events) {
+            try {
+                outboxService.markAsProcessing(event);
+
+                // TODO Publish the event
+
+                successfulEvents++;
+                outboxService.markAsPublished(event);
+            } catch (Exception e) {
+                failedEvents++;
+                outboxService.handleFailure(event, e);
+            }
+        }
+
+        log.debug("Outbox scheduled job completed in {}ms. successfulEvents={} failedEvents={}", Duration.between(start, now()).toMillis(), successfulEvents, failedEvents);
+    }
+}
