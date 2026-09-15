edac62d1ee3e357b2b33173ff7d93669f97f3507
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-09-15 16:46:13 +0200

    FEAT: Add ProcessedEvent to keep track of already processed kafka events

---
+++ services/catalog-query-service/src/main/java/it/eufonica/catalogqueryservice/processing/ProcessedEvent.java
@@ -0,0 +1,27 @@
+package it.eufonica.catalogqueryservice.processing;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor @NoArgsConstructor
+@Getter @Setter @Builder
+@ToString @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class ProcessedEvent {
+    @EqualsAndHashCode.Include
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String eventType;
+
+    @Column(nullable = false)
+    private String aggregateId;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private LocalDateTime processedAt = LocalDateTime.now();
+}
