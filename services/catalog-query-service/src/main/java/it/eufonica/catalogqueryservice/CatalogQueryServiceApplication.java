621d9dae01bf2c40667e1cc532e838990dcb54da
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-09-15 12:18:02 +0200

    FEAT: Initialize catalog query service with basic configuration and structure

---
+++ services/catalog-query-service/src/main/java/it/eufonica/catalogqueryservice/CatalogQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package it.eufonica.catalogqueryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogQueryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogQueryServiceApplication.class, args);
+    }
+
+}
---
+++ services/catalog-query-service/src/test/java/it/eufonica/catalogqueryservice/CatalogQueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.eufonica.catalogqueryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogQueryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
