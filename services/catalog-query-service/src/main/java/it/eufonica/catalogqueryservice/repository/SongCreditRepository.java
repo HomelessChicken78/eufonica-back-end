8c463ae53b071df96720e0acc2e489fb9f809813
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-09-22 15:02:42 +0200

    FEAT: Add method to find SongCredits by songId in SongCreditRepository

--- services/catalog-query-service/src/main/java/it/eufonica/catalogqueryservice/repository/SongCreditRepository.java
+++ services/catalog-query-service/src/main/java/it/eufonica/catalogqueryservice/repository/SongCreditRepository.java
@@ -3,9 +3,12 @@ package it.eufonica.catalogqueryservice.repository;
 import it.eufonica.catalogqueryservice.model.SongCreditRead;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface SongCreditRepository extends JpaRepository<SongCreditRead, UUID> {
+    List<SongCreditRead> findBySongId(UUID songId);
+
     void deleteBySongId(UUID songId);
 
     void deleteBySongIdAndArtistId(UUID songId, UUID artistId);
