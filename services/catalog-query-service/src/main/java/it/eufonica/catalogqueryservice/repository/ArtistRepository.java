0a64ead753a11a7278c73ef4a062a4767963e228
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-09-24 15:56:35 +0200

    FEAT: Add pagination support to ArtistRepository with findByNameContainingIgnoreCase method

--- services/catalog-query-service/src/main/java/it/eufonica/catalogqueryservice/repository/ArtistRepository.java
+++ services/catalog-query-service/src/main/java/it/eufonica/catalogqueryservice/repository/ArtistRepository.java
@@ -1,9 +1,12 @@
 package it.eufonica.catalogqueryservice.repository;
 
 import it.eufonica.catalogqueryservice.model.ArtistRead;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface ArtistRepository extends JpaRepository<ArtistRead, UUID> {
+    Page<ArtistRead> findByNameContainingIgnoreCase(String name, Pageable page);
 }
