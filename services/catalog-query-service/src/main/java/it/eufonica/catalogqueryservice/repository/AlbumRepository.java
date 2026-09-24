42cd885518b1d2a4abe2894cc5dadf9ea3a23ec9
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-09-24 10:12:27 +0200

    FEAT: Add pageable specification method to AlbumRepository for album retrieval

--- services/catalog-query-service/src/main/java/it/eufonica/catalogqueryservice/repository/AlbumRepository.java
+++ services/catalog-query-service/src/main/java/it/eufonica/catalogqueryservice/repository/AlbumRepository.java
@@ -1,9 +1,13 @@
 package it.eufonica.catalogqueryservice.repository;
 
 import it.eufonica.catalogqueryservice.model.AlbumRead;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface AlbumRepository extends JpaRepository<AlbumRead, UUID> {
+    Page<AlbumRead> findAll(Specification<AlbumRead> spec, Pageable pageable);
 }
