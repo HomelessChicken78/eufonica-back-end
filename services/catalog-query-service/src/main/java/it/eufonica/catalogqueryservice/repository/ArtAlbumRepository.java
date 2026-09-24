a1a90b64c1a10958a68d5133732a0c8af49879ad
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-09-24 10:04:28 +0200

    FEAT: Add method to find ArtAlbumRead by albumId in ArtAlbumRepository

--- services/catalog-query-service/src/main/java/it/eufonica/catalogqueryservice/repository/ArtAlbumRepository.java
+++ services/catalog-query-service/src/main/java/it/eufonica/catalogqueryservice/repository/ArtAlbumRepository.java
@@ -3,8 +3,11 @@ package it.eufonica.catalogqueryservice.repository;
 import it.eufonica.catalogqueryservice.model.ArtAlbumRead;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ArtAlbumRepository extends JpaRepository<ArtAlbumRead, UUID> {
     void deleteByAlbumId(UUID albumId);
+
+    List<ArtAlbumRead> findByAlbumId(UUID albumId);
 }
