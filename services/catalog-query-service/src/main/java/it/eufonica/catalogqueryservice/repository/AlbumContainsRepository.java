9e358c38625fe7109f1fd5774f74854b6cc55c41
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-09-24 10:06:04 +0200

    FEAT: Add method to find AlbumContainsRead by albumId in AlbumContainsRepository

--- services/catalog-query-service/src/main/java/it/eufonica/catalogqueryservice/repository/AlbumContainsRepository.java
+++ services/catalog-query-service/src/main/java/it/eufonica/catalogqueryservice/repository/AlbumContainsRepository.java
@@ -3,6 +3,7 @@ package it.eufonica.catalogqueryservice.repository;
 import it.eufonica.catalogqueryservice.model.AlbumContainsRead;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AlbumContainsRepository extends JpaRepository<AlbumContainsRead, UUID> {
@@ -11,4 +12,6 @@ public interface AlbumContainsRepository extends JpaRepository<AlbumContainsRead
     void deleteByAlbumIdAndSongId(UUID albumId, UUID songId);
 
     boolean existsByAlbumIdAndSongId(UUID albumId, UUID songId);
+
+    List<AlbumContainsRead> findByAlbumId(UUID albumId);
 }
