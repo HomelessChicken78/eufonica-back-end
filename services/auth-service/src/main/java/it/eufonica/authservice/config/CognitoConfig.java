1124b363e51490e0ff6775bbc5d19a41f2770a49
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-09-25 15:45:30 +0200

    CHORE: Add Cognito Bean Configuration class

---
+++ services/auth-service/src/main/java/it/eufonica/authservice/config/CognitoConfig.java
@@ -0,0 +1,20 @@
+package it.eufonica.authservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
+
+@Configuration
+public class CognitoConfig {
+    @Value("${AWS_REGION}")
+    private String region;
+
+    @Bean
+    public CognitoIdentityProviderClient cognitoIdentityProviderClient() {
+        return CognitoIdentityProviderClient.builder()
+                .region(Region.of(region))
+                .build();
+    }
+}
