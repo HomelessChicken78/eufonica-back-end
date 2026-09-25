c6e0ba0bb4fc47464f5acf675ac29b12aa3314b4
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-09-25 17:15:04 +0200

    FEAT: Add exception and handlers

---
+++ services/auth-service/src/main/java/it/eufonica/authservice/exception/server/HttpServerErrorException.java
@@ -0,0 +1,25 @@
+package it.eufonica.authservice.exception.server;
+
+import it.eufonica.authservice.exception.HttpException;
+
+public class HttpServerErrorException extends HttpException {
+    public HttpServerErrorException(String message) {
+        super(message);
+    }
+
+    public HttpServerErrorException() {
+        super();
+    }
+
+    public HttpServerErrorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public HttpServerErrorException(Throwable cause) {
+        super(cause);
+    }
+
+    protected HttpServerErrorException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
